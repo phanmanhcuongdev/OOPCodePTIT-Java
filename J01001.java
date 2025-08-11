2f99ce259ec744055f2f38b49354817fc45d09e3
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-08-11 14:53:54 +0700

    Create J01001.java

---
+++ J01001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RECTANGLE {
+    // tính diện tích và chu vi
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float x = scanner.nextFloat();
+        float y = scanner.nextFloat();
+        scanner.close();
+        if(x<=0|| y<=0) System.out.println("0");
+        else System.out.printf("%.0f %.0f\n",(x+y)*2,x*y);
+    }
+}
